6e8a108ab9a81bfbf85da1d2c93e636b7b872058
Seth Todd <seth.todd@outlook.com>
2020-02-09 23:54:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]
+programmer_hash[yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]}
 
 end
 
